6f742195d69accac9bb575a7b2e810ab01e7e1d8
Daniel Casanovas <danius4@gmail.com>
2014-09-19 22:52:04 +0200

    First Commit

---
+++ src/solverassistant/SolverAssistant.java
@@ -0,0 +1,33 @@
+/*
+ * TFG: Daniel Casanovas Gayoso 
+ * Grau en Enginyeria Informàtica - Escola Politècnica Superior de Lleida
+ * 2014/2015
+ */
+package solverassistant;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SolverAssistant extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
